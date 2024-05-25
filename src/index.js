4daea0f4992692277abc057731141a7d1f7fee4b
Ben <luben12337@gmail.com>
2024-05-25 23:04:11 +1000

    Changed some card text

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const grid = document.querySelector('.grid');
 document.querySelector('.headshot').appendChild(profilePic);
 
 // create project cards
-grid.appendChild(createCard(todoThumb, 'To-Do', 'A simple to do app similar to the Microsoft to-do website.', 'https://github.com/benl12337/todo2', 'https://youtube.com'));
+grid.appendChild(createCard(todoThumb, 'To-Do', 'A simple to do app in the style of the Microsoft to-do website.', 'https://github.com/benl12337/todo2', 'https://youtube.com'));
 grid.appendChild(createCard(battleshipThumb, 'Battleship', 'A simple turn-based web version of the classic Battleship game.', 'https://github.com/benl12337/todo2', 'https://youtube.com'));
 grid.appendChild(createCard(wthrThumb, 'WTHR.', 'A simple weather app built on the the weather API.', 'https://github.com/benl12337/todo2', 'https://youtube.com'));
 grid.appendChild(createCard(tictactoeThumb, 'Tic-Tac-Toe', 'A simple turn-based web version of the classic Tic Tac Toe game.', 'https://github.com/benl12337/todo2', 'https://youtube.com'));
